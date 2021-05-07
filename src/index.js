9cf5264052dfebb03996ab6f96896a347dfa5fce
Robyn Noble <robynhurd09@gmail.com>
2021-05-07 06:45:31 -0500

    setup index file, basic necessities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store=createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(
+        logger
+    )
+);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
 registerServiceWorker();
